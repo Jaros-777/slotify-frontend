a66de71d83e697ee5d0489248cd51e2fa8e75060
Jaros <filip.jarocki@wp.pl>
2025-10-03 17:28:09 +0200

    chore: update node and add react-router-dom

---
+++ src/pages/Auth/Auth.tsx
@@ -0,0 +1,9 @@
+
+const Auth =()=>{
+
+    return(
+        <section>
+            <p>hej</p>
+        </section>
+    )
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,20 @@
 import './Home.css'
 import { Navbar } from '../../components/Navbar/Navbar'
 import { Hero } from './components/Hero'
-import {Features} from "./components/Features"
+import { Features } from "./components/Features"
 import { Footer } from '../../components/Footer/Footer'
 
 function Home() {
 
   return (
     <>
-    <Navbar/>
-    <Hero/>
-    <Features/>
-    <Footer/>
+      <Navbar />
+      <main>
+        <Hero />
+        <Features />
+        <Footer />
+      </main>
+
     </>
   )
 }
