2a9ef3b1fc43742f494eac1657e94e2a56123759
Jaros <filip.jarocki@wp.pl>
2025-11-14 17:37:31 +0100

    feat(client, business-panel): continue create UI

--- src/pages/Admin/components/Clients/Reservation.tsx
+++ src/pages/Admin/components/Clients/Reservation.tsx
@@ -1,9 +1,11 @@
 import { useParams } from "react-router-dom"
-import { Image, ExternalLink } from "lucide-react"
+import { Image, Share2, Globe } from "lucide-react"
 import { useEffect, useState } from "react"
 import type { ServiceType } from "../types/ServiceType"
 import axios from "axios"
 import type { BusinessProfileType } from "../Booking/components/BusinessProfile/types/BusinessProfileType"
+import { FooterAdmin } from "../../../../components/Footer/FooterAdmin"
+import FacebookLogo from "../Booking/components/assets/Facebook_Logo_Primary.png"
 
 
 export const Reservation = () => {
@@ -18,7 +20,6 @@ export const Reservation = () => {
 
         await axios.get(`http://localhost:8080/business-page/${businessName}`)
             .then((response) => {
-                console.log(response.data)
                 setBusinessDetail(response.data.businessProfileDTO)
                 const serData = response.data.servicesDTO
                 setServiceData(serData.filter((service: { isEditable: boolean }) => service.isEditable === true))
@@ -31,7 +32,6 @@ export const Reservation = () => {
 
     useEffect(() => {
         (async () => {
-            // await loadServiceData(token);
             await handleValidPage()
 
         })();
@@ -43,7 +43,7 @@ export const Reservation = () => {
 
     return (
         <>
-            <nav className="border-b border-gray-300 flex justify-around px-4 py-4">
+            <nav className="border-b border-gray-300 flex justify-between px-40 py-6">
                 <p className="font-bold">{businessDetail.businessName}</p>
                 <div>
                     {/* <CircleUserRound/> */}
@@ -51,66 +51,118 @@ export const Reservation = () => {
                 </div>
 
             </nav>
-            <section className="flex flex-col p-4 border-b border-gray-300 shadow-2xl">
-                <div className="flex justify-between px-4 py-2">
-                    <div className="flex mx-4 items-center">
-                        <div className="bg-gray-200 p-8 rounded-2xl">
-                            <Image></Image>
-                        </div>
-                        <div className="ml-6">
-                            <p className="font-bold text-xl">{businessDetail.businessName}</p>
-                            <p className="text-sm"><span className="text-green-700 font-bold">Opening hours </span>(8:00-20:00)</p>
-                        </div>
-                    </div>
-                    <div className="flex flex-col justify-center items-center">
-                        <div className="bg-gray-200 p-4 rounded-full">
-                            <ExternalLink></ExternalLink>
-                        </div>
-                        <p className="mt-2">Share</p>
-                    </div>
-                </div>
-                <div className="mt-6 px-4 flex">
-                    <button className="font-medium cursor-pointer">Services</button>
-                    <button className="ml-8 font-medium cursor-pointer">About</button>
-                </div>
-
-            </section>
-            <section className="mt-10 p-4">
-                <p className="text-2xl font-bold">Services</p>
-                <ul className="mt-4 border border-gray-300 rounded-md">
-                    {serviceData.map((e) => (
-                        <li key={e?.id} className="flex p-4 border-b border-gray-300 items-center">
-                            <div className="h-24 w-24 bg-green-100 p-2 rounded-2xl">
-                                <Image className="h-full w-full text-green-600"></Image>
+            <div className="w-full flex flex-col items-center ">
+                <section className="flex flex-col items-center w-full p-4 pb-8 border-b border-gray-300 shadow-2xl">
+                    <div className="flex justify-between px-4 py-2 w-[80rem]">
+                        <div className="flex mx-4 items-center">
+                            <div className="bg-gray-200 p-8 rounded-2xl">
+                                <Image></Image>
                             </div>
                             <div className="ml-6">
-                                <p className="font-bold text-lg">{e?.name}</p>
-                                <p className="text-sm underline cursor-pointer mt-2">Show details</p>
+                                <p className="font-bold text-xl">{businessDetail.businessName}</p>
+                                <p className="text-sm"><span className="text-green-700 font-bold">Opening hours </span>(8:00-20:00)</p>
                             </div>
-                            <div className="ml-auto flex items-center">
-                                <p className="font-bold">{e?.price} USD</p>
-                                <button className="border border-blue-300 text-blue-400 font-bold px-4 py-2 rounded-md ml-4 cursor-pointer duration-200 
-                                hover:bg-blue-200">Reservation</button>
+                        </div>
+                        {/* <div className="flex flex-col justify-center items-center">
+                            <div className="bg-gray-200 p-4 rounded-full">
+                                <Share2></Share2>
                             </div>
-                        </li>
-                    ))}
-                </ul>
-
-            </section>
-            <section>
-                <div>
-                    <h2>About</h2>
-                </div>
-                <div>
-                    <h2>Contact details</h2>
-                    <div>
-                        <a href="#">Web Page</a>
-                        <a href="#">
-                            <p>Facebook</p>
-                        </a>
+                            <p className="mt-2">Share</p>
+                        </div> */}
                     </div>
-                </div>
-            </section>
+
+                </section>
+                <section className="mt-10 p-4 w-[80rem]">
+                    <p className="text-2xl font-bold">Services</p>
+                    <ul className="mt-4 border border-gray-300 rounded-md">
+                        {serviceData.map((e) => (
+                            <li key={e?.id} className="flex p-4 border-b border-gray-300 items-center">
+                                <div className="h-24 w-24 bg-green-100 p-2 rounded-2xl">
+                                    <Image className="h-full w-full text-green-600"></Image>
+                                </div>
+                                <div className="ml-6">
+                                    <p className="font-bold text-lg">{e?.name}</p>
+                                    <p className="text-sm underline cursor-pointer mt-2">Show details</p>
+                                </div>
+                                <div className="ml-auto flex items-center">
+                                    <p className="font-bold">{e?.price} USD</p>
+                                    <button className="border border-blue-300 text-blue-400 font-bold px-4 py-2 rounded-md ml-4 cursor-pointer duration-200 
+                                hover:bg-blue-200">Reservation</button>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+
+                </section>
+                <section className="px-6 py-4 w-[80rem]">
+                    <div>
+                        <h2 className="text-2xl font-bold">About</h2>
+                    </div>
+                    <div className="flex justify-between">
+                        <div>
+
+                            {businessDetail.slogan ?
+                                <div className="w-1/2">
+                                    <p className="font-bold mt-8 text-lg">Slogan</p>
+                                    <p className="mt-4">{businessDetail.slogan}</p>
+                                </div>
+                                : null}
+                            {businessDetail.description ?
+                                <div className="w-1/2">
+                                    <p className="font-bold mt-8 text-lg">Who we are</p>
+                                    <p className="mt-4">{businessDetail.description}</p>
+                                </div>
+                                : null}
+                        </div>
+                        <div className="">
+                            {businessDetail.websiteURL || businessDetail.facebookURL ?
+                                <div>
+                                    <h2 className="font-bold mt-4 text-lg">Contact details</h2>
+                                    <div className="flex mt-4">
+                                        {businessDetail.websiteURL ?
+                                            <a href={businessDetail.websiteURL} className="flex items-center" target="_blank">
+                                                <Globe className="h-5"></Globe>
+                                                <p className="ml-4 underline text-nowrap">Web Page</p>
+                                            </a>
+                                            : null
+                                        }
+                                        {businessDetail.facebookURL ?
+                                            <a href={businessDetail.facebookURL} className="flex ml-6" target="_blank">
+                                                <img src={FacebookLogo} alt="Facebok logo" className="h-5" />
+                                                <p className="ml-4 underline text-nowrap">Facebook page</p>
+                                            </a>
+                                            : null
+                                        }
+
+                                    </div>
+                                </div>
+                                : null}
+                            <h2 className="font-bold mt-10 text-lg">Opening hours</h2>
+                            <div className="flex">
+                                <div className="w-30 mt-2">
+                                    <p className="mt-2 font-medium">Monday</p>
+                                    <p className="mt-2 font-medium">Tuesday</p>
+                                    <p className="mt-2 font-medium">Wednesday</p>
+                                    <p className="mt-2 font-medium">Thursday</p>
+                                    <p className="mt-2 font-medium">Friday</p>
+                                    <p className="mt-2 font-medium">Saturday</p>
+                                    <p className="mt-2 font-medium">Sunday</p>
+                                </div>
+                                <div className="mt-2">
+                                    <p className="mt-2">08:00 - 20:00</p>
+                                    <p className="mt-2">08:00 - 20:00</p>
+                                    <p className="mt-2">08:00 - 20:00</p>
+                                    <p className="mt-2">08:00 - 20:00</p>
+                                    <p className="mt-2">08:00 - 20:00</p>
+                                    <p className="mt-2">08:00 - 20:00</p>
+                                    <p className="mt-2">08:00 - 20:00</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <FooterAdmin></FooterAdmin>
         </>
     )
 }
