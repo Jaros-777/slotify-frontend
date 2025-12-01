98d58251c31d9d11230e87633c9b74f8277af1aa
Jaros <filip.jarocki@wp.pl>
2025-12-01 19:56:47 +0100

    feat(admin,settings,availabilty): done UI

--- src/pages/Admin/components/Settings/components/Availability/Availability.tsx
+++ src/pages/Admin/components/Settings/components/Availability/Availability.tsx
@@ -1,38 +1,50 @@
 import { useEffect, useState } from "react";
 import { useCheckIsLogged } from "../../../utlis/checkIsLoged";
-import type { scheduleWeek } from "./utlis/scheduleType";
+import type { scheduleDay } from "./utlis/scheduleType";
 
 
-const initialSchedule: scheduleWeek = {
-  monday: { openHour: "08:00", closeHour: "20:00", isClose: false },
-  tuesday: { openHour: "08:00", closeHour: "20:00", isClose: false },
-  wednesday: { openHour: "08:00", closeHour: "20:00", isClose: false },
-  thursday: { openHour: "08:00", closeHour: "20:00", isClose: false },
-  friday: { openHour: "08:00", closeHour: "20:00", isClose: false },
-  saturday: { openHour: "10:00", closeHour: "18:00", isClose: true },
-  sunday: { openHour: "10:00", closeHour: "18:00", isClose: true },
-};
+const initialSchedule: scheduleDay[] = [
+    { id: 0, dayOfWeek: 0, openHour: "08:00", closeHour: "18:00", isClose: false },
+    { id: 1, dayOfWeek: 1, openHour: "08:00", closeHour: "18:00", isClose: false },
+    { id: 2, dayOfWeek: 2, openHour: "08:00", closeHour: "18:00", isClose: false },
+    { id: 3, dayOfWeek: 3, openHour: "08:00", closeHour: "18:00", isClose: false },
+    { id: 4, dayOfWeek: 4, openHour: "08:00", closeHour: "18:00", isClose: false },
+    { id: 5, dayOfWeek: 5, openHour: "10:00", closeHour: "18:00", isClose: true },
+    { id: 6, dayOfWeek: 6, openHour: "10:00", closeHour: "18:00", isClose: true }
 
-type dayTypes = "monday"
+];
+
+const dayTypes = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+]
 
 
 export const Availability = () => {
     const { checkIsLogged, isAuthLoading } = useCheckIsLogged();
-    const [schedulePlan, setSchedulePlan] = useState<scheduleWeek>(initialSchedule)
+    const [schedulePlan, setSchedulePlan] = useState<scheduleDay[]>(initialSchedule)
 
-    const handleChangeSchedule =(day: dayTypes, field: keyof scheduleWeek, value: string | boolean)=>{
-        setSchedulePlan(prev=>({
-            ...prev,
-            [day]: {...prev[day], [field]:value}
-        }))
+    const handleChangeSchedule = (dayOfWeek: number, field: "openHour" | "closeHour" | "isClose", value: string | boolean) => {
+        setSchedulePlan(prev => 
+            prev.map(day=>
+                day.dayOfWeek === dayOfWeek ?
+                {...day, [field]:value} :
+                day
+            )
+        )
     }
 
 
     useEffect(() => {
-            (async () => {
-                await checkIsLogged();
-            })();
-        }, []);
+        (async () => {
+            await checkIsLogged();
+        })();
+    }, []);
 
     if (isAuthLoading) {
         return <p className="mt-20">Checking authentication...</p>;
@@ -46,42 +58,50 @@ export const Availability = () => {
                     <h1 className="text-3xl font-bold">Availability</h1>
                 </div>
             </div>
-            <div className="flex flex-col my-2 w-[90%] bg-white p-4 rounded-md cursor-pointer">
+            <div className="flex flex-col my-2 w-[90%] bg-white p-4 rounded-md">
                 <h3 className="font-bold">Opening hours</h3>
                 <p className="border-b border-gray-300 pb-6 pt-2">Adjust the general operating hours of your business.</p>
-                <div>
-                    <div className="flex justify-around p-4">
-                        <div className="flex">
-                            <input type="checkbox" />
-                            <p className="ml-4 font-bold">Monday</p>
-                        </div>
-                        <div className="flex">
-                            <p className="font-bold mr-4">FROM</p>
-                            <input 
-                            type="time" 
-                            className="mr-6"
-                            value={schedulePlan.monday.openHour}
-                            // onChange={(e)=>handleChangeSchedule("monday","openHour", e.target.value)}
-                            />
-                            <p className="font-bold mr-4">TO</p>
-                            <input type="time" />
-                        </div>
-                    </div>
-                    <div className="flex justify-around p-4">
-                        <div className="flex">
-                            <input type="checkbox" />
-                            <p>Monday</p>
-                        </div>
-                        <div className="flex">
-                            <p>FROM</p>
-                            <input type="time" />
-                            <p>FROM</p>
-                            <input type="time" />
-                        </div>
-                    </div>
-                </div>
+                <ul>
+                    {schedulePlan.map((e) => (
+                        <li className="flex p-4" key={e.id}>
+                            <div className="flex w-1/2">
+                                <input
+                                    className="cursor-pointer"
+                                    type="checkbox"
+                                    checked = {e.isClose}
+                                    onChange={(ev) => handleChangeSchedule(e.dayOfWeek, "isClose", ev.target.checked)}
+                                />
+                                <p className="ml-4 font-medium">{dayTypes[e.dayOfWeek]}</p>
+                            </div>
+                            {e.isClose ?
+                                <div>
+                                    <p className="font-medium">Closed</p>
+                                </div>
+                                :
+                                <div className="flex w-1/2">
+                                    <p className="font-medium mr-8">FROM</p>
+                                    <input
+                                        type="time"
+                                        className="mr-6 cursor-pointer"
+                                        value={schedulePlan[e.dayOfWeek].openHour}
+                                        onChange={(ev) => handleChangeSchedule(e.dayOfWeek, "openHour", ev.target.value)}
+                                    />
+                                    <p className="font-medium mr-4">TO</p>
+                                    <input
+                                        type="time"
+                                        className="mr-6 cursor-pointer"
+                                        value={schedulePlan[e.dayOfWeek].closeHour}
+                                        onChange={(ev) => handleChangeSchedule(e.dayOfWeek, "closeHour", ev.target.value)}
+                                    />
+                                </div>
+                            }
+
+                        </li>
+                    ))}
+
+                </ul>
             </div>
-            
+
         </div>
 
     )
--- src/pages/Admin/components/Settings/components/Availability/utlis/scheduleType.ts
+++ src/pages/Admin/components/Settings/components/Availability/utlis/scheduleType.ts
@@ -1,16 +1,18 @@
 
-interface scheduleDay {
+export interface scheduleDay {
+    id: number,
+    dayOfWeek: number,
     openHour: string,
     closeHour: string,
     isClose: boolean,
 }
 
-export interface scheduleWeek {
-    monday: scheduleDay,
-    tuesday: scheduleDay,
-    wednesday: scheduleDay,
-    thursday: scheduleDay,
-    friday: scheduleDay,
-    saturday: scheduleDay,
-    sunday: scheduleDay,
-}
+// export interface scheduleWeek [
+//     {monday: scheduleDay},
+//     {tuesday: scheduleDay},
+//     wednesday: scheduleDay,
+//     thursday: scheduleDay,
+//     friday: scheduleDay,
+//     saturday: scheduleDay,
+//     sunday: scheduleDay,
+// ]
