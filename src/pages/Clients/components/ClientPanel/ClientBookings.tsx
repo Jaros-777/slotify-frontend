fb7946bd39b13daa98e329454da4f5a14e43d516
Jaros <filip.jarocki@wp.pl>
2026-01-01 14:55:32 +0100

    feat(client, panel, bookings): complete client booking section

--- src/pages/Clients/components/ClientPanel/ClientBookings.tsx
+++ src/pages/Clients/components/ClientPanel/ClientBookings.tsx
@@ -5,19 +5,43 @@ import axios from "axios"
 import { useCheckIsLogged } from "../../../Admin/components/utlis/checkIsLoged"
 import { LoadingPage } from "../../../../LoadingPage"
 import { monthTypes } from "../Order/types/dayAndMonthNames"
+import { useNavigate } from "react-router-dom"
 
 export const ClientBookings = () => {
     const { checkIsLogged, isAuthLoading } = useCheckIsLogged("client");
-    const [bookings, setBookings] = useState<bookingType[]>([])
+    const [loadBookings, setLoadBookings] = useState<Boolean>(true)
+    const [futureBookings, setFutureBookings] = useState<bookingType[]>([])
+    const [previousBookings, setPreviousBookings] = useState<bookingType[]>([])
+    const [selectedTime, setSelectedTime] = useState<"prev" | "future">("future")
+    const bookings = selectedTime === "future" ? futureBookings : previousBookings;
+    const navigate = useNavigate()
 
+
+    const splitBookingsByDate = (bookings: bookingType[]) => {
+
+        const date = new Date();
+        const previous: bookingType[] = [];
+        const future: bookingType[] = [];
+
+        bookings.map(e => {
+            const eventDate = new Date(e.date);
+            if (eventDate <= date) {
+                previous.push(e)
+            } else {
+                future.push(e)
+            }
+        })
+        setPreviousBookings(previous)
+        setFutureBookings(future)
+    }
 
     const handleFetchData = async (token: string) => {
         await axios.get(`${import.meta.env.VITE_APP_URL}/client/bookings`,
             {
                 headers: { Authorization: `Bearer ${token}` },
             }).then(response => {
-                console.log(response.data)
-                setBookings(response.data)
+                setLoadBookings(false)
+                splitBookingsByDate(response.data)
             }).catch(error => {
                 console.log(error)
             });
@@ -36,7 +60,7 @@ export const ClientBookings = () => {
         return <LoadingPage text="Checking authentication..." ></LoadingPage>;
     }
 
-    if (!bookings) {
+    if (loadBookings) {
         return <LoadingPage text="Loading data..." ></LoadingPage>;
     }
 
@@ -44,36 +68,44 @@ export const ClientBookings = () => {
         <div className="p-4 flex flex-col items-center">
             <h1 className="text-2xl font-medium min-w-300">Bookings</h1>
             <div className="text-lg my-4 min-w-300">
-                <button className="py-2 pr-4 font-medium cursor-pointer">UPCOMING</button>
-                <button className="py-2 px-4 font-medium cursor-pointer text-blue-500 underline">HISTORY</button>
+                <button className={`py-2 px-4 font-medium cursor-pointer ${selectedTime === "future" ? "text-blue-500 underline" : null} `} onClick={() => setSelectedTime("future")}>UPCOMING</button>
+                <button className={`py-2 px-4 font-medium cursor-pointer ${selectedTime === "prev" ? "text-blue-500 underline" : null} `} onClick={() => setSelectedTime("prev")}>HISTORY</button>
             </div>
             <ul className="min-w-300">
-                {bookings.map(e => {
-                    const date = new Date(e.date);
-                    return (
-                        <li key={e.eventId} className="flex items-center border border-gray-300 rounded-2xl mb-2">
-                            <div className="flex flex-col items-center justify-center py-2 w-26">
-                                <p className="font-medium">{monthTypes[date.getMonth()]}</p>
-                                <p className="font-bold">{date.getDay()}</p>
-                                <p className="font-medium">{String(date.getHours()).padStart(2, "0")}:{String(date.getMinutes()).padStart(2, "0")}</p>
+                {bookings.length === 0 ?
+                    selectedTime === "future" ?
+                        <p className="ml-4">You don’t have any upcoming bookings.</p>
+                        :
+                        <p className="ml-4">You don’t have history bookings.</p>
+                    :
+                    bookings.map(e => {
+                        const date = new Date(e.date);
+                        return (
 
-                            </div>
-                            <div className="flex border-l border-gray-300 h-full py-4 pl-6">
-                                {e.businessProfilePicURL ?
-                                    <img className="h-20 overflow-hidden rounded-full flex items-center justify-center" src={e.businessProfilePicURL} alt="Business profile picture" />
-                                    :
-                                    <Image className="h-20 aspect-square text-gray-400" />}
-                            </div>
-                            <div className="ml-4 p-4">
-                                <p className="font-bold">{e.serviceName}</p>
-                                <p className="mt-2">{e.businessName}</p>
-                            </div>
-                            <div className="ml-auto py-4 pr-8">
-                                <p className="font-bold">{e.servicePrice} USD</p>
-                            </div>
-                        </li>
-                    )
-                })}
+                            <li key={e.eventId} className="flex items-center border border-gray-300 rounded-2xl mb-2">
+                                <div className="flex flex-col items-center justify-center py-2 w-26">
+                                    <p className="font-medium">{monthTypes[date.getMonth()]}</p>
+                                    <p className="font-bold">{date.getDay()}</p>
+                                    <p className="font-medium">{String(date.getHours()).padStart(2, "0")}:{String(date.getMinutes()).padStart(2, "0")}</p>
+
+                                </div>
+                                <div className="flex border-l border-gray-300 h-full py-4 pl-6 cursor-pointer" onClick={() => navigate(`/${e.businessName}`)}>
+                                    {e.businessProfilePicURL ?
+                                        <img className="h-20 overflow-hidden rounded-full flex items-center justify-center" src={e.businessProfilePicURL} alt="Business profile picture" />
+                                        :
+                                        <Image className="h-20 aspect-square text-gray-400" />}
+                                </div>
+                                <div className="ml-4 p-4">
+                                    <p className="font-bold">{e.serviceName}</p>
+                                    <p className="mt-2">{e.businessName}</p>
+                                </div>
+                                <div className="ml-auto py-4 pr-8">
+                                    <p className="font-bold">{e.servicePrice} USD</p>
+                                </div>
+                            </li>
+                        )
+                    })
+                }
             </ul>
         </div>
     )
