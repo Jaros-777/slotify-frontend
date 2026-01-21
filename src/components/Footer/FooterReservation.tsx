0e7dc96a7860600d0402ff1e9789ca8a8ecdb566
Jaros <filip.jarocki@wp.pl>
2026-01-21 11:40:19 +0100

    feat(client,reservation,footer): add mobile view

--- src/components/Footer/FooterReservation.tsx
+++ src/components/Footer/FooterReservation.tsx
@@ -6,12 +6,12 @@ export const FooterReservation = () => {
 
     return (
         <footer className="mt-8 border-t border-gray-300 flex flex-col items-center w-full">
-            <div className="h-1/2 w-full max-w-[80rem] flex justify-between py-6 px-8 ">
+            <div className="h-1/2 w-full max-w-[80rem] flex flex-col sm:flex-row  justify-between py-6 px-8 ">
                 <div className="h-full flex items-center justify-center">
                     <p>Powered by</p>
                     <img className="w-26 ml-2" src={Logo} alt="Slotify Logo" />
                 </div>
-                <p className="text-center">© 2025 Jaros. Made with ❤️</p>
+                <p className="text-center mt-4 sm:mt-0">© 2025 Jaros. Made with ❤️</p>
             </div>
             <div className="bg-blue-500 p-4 w-full">
                 <p className="text-center text-white">Got your own business? <span className="underline font-bold cursor-pointer" onClick={() => {navigate("/register/business"); window.scrollTo(0,0)}}>Try Slotify</span> and create your own Booking Website</p>
